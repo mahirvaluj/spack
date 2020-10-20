eeeed048c523e1826a5b19b7df9dcf72de5fc364
Sean Maher <seanptmaher@gmail.com>
2020-10-20 00:43:28 +0000

    var+types->buffer, buffer->var+types except arr+group

--- leb128.lisp
+++ leb128.lisp
@@ -6,7 +6,8 @@
 (in-package :leb128)
 
 (defun encode-signed (i)
-  "Encode an integer into a leb128 unsigned-8 buffer"
+  "Encode a 64-bit integer integer into a leb128 unsigned-8 buffer"
+  (declare ((signed-byte 64) i))
   (let ((more t) (curr) (ret (make-array 16 :element-type '(unsigned-byte 8) :adjustable t :fill-pointer 0))) ;(neg (< i 0))
     (loop while more do
          ;; (format t "i: ~A ~%" i)
@@ -20,6 +21,16 @@
          (vector-push-extend curr ret))
     ret))
 
-(defun decode-signed (buf)
+(defun decode-signed (buf &key (start 0))
   "decode integer from buffer"
-  (error "not implemented yet"))
+  (let ((result 0) (shift 0) (curr) (cont t) (counter 0))
+    (loop while cont do 
+         (setf curr (aref buf start))
+         (setf start (+ 1 start))
+         (setf result (logior result (ash (logand curr #x7f) shift)))
+         (setf shift (+ 7 shift))
+         (incf counter)
+         (when (= 0 (logand curr #x80))
+           (if (= 64 (logand curr #x40))
+               (return-from decode-signed (values (logior result (ash (lognot 0) shift)) counter))
+               (return-from decode-signed (values result counter)))))))
--- spack.lisp
+++ spack.lisp
@@ -50,6 +50,14 @@
                         :val (trivial-utf-8:string-to-utf-8-bytes elem))
          :spack-elem packet))
 
+(defmethod spush ((elem array) (type (eql :string)) (packet spack))
+  "Push a string already encoded as utf-8 onto spack"
+  (loop for i across elem do (assert (and (> i 0) (< i 256))))
+  (spush (make-instance 'spack-elem
+                        :elem-type :string
+                        :val elem)
+         :spack-elem packet))
+
 (defmethod spush ((elem array) (type (eql :array)) (packet spack))
   "Push an array onto spack, all elements must have the same type (can
    be struct)"
@@ -83,16 +91,16 @@
                                       elembuf)))
            ((eq (elem-type elem) :float64)
             (progn
-              (vector-push-extend #x02 typebuf)
+              (vector-push-extend #x03 typebuf)
               (vector-push-buf-extend (cl-intbytes:int64->octets (val elem))
                                       elembuf)))
            ((eq (elem-type elem) :byte)
             (progn
-              (vector-push-extend #x03 typebuf)
+              (vector-push-extend #x04 typebuf)
               (vector-push-extend (val elem) elembuf)))
            ((eq (elem-type elem) :string)
             (progn
-              (vector-push-extend #x04 typebuf)
+              (vector-push-extend #x05 typebuf)
               (vector-push-buf-extend (leb128:encode-signed (length (val elem)))
                                       typebuf)
               (vector-push-buf-extend (val elem)
@@ -101,7 +109,8 @@
             (error "not implemented yet")
             )
            ((eq (elem-type elem) :group)
-            (error "not implemented yet"))))
+            (error "not implemented yet"))
+           (t (error (format nil "bad type ~A passed to out" (elem-type elem))))))
     ;; NOTE: This is badly done, but ironclad doesn't support non-simple vectors??
     (let ((buf (concatenate '(vector (unsigned-byte 8))
                             (leb128:encode-signed (length typebuf))
@@ -112,3 +121,84 @@
                    (ironclad:digest-sequence :sha256 buf)
                    buf))))
 
+(defun verify-sha-integrity (buf)
+  "Verifies sha-256 at start of spack packet"
+  (let ((digest (ironclad:digest-sequence :sha256 buf :start 32)))
+    (loop for i from 0 below (length digest)
+       do (unless (= (aref buf i) (aref digest i)) (return-from verify-sha-integrity nil)))
+    t))
+
+(defmacro with-gensyms (syms &body body)
+  `(let ,(loop for s in syms collect `(,s (gensym)))
+    ,@body))
+
+(defmacro parse-and-increment (capture-var index &body parse-body)
+  "Parse and increment expects a function to return at least two
+things. A value, and an integer"
+  (with-gensyms (parsed val)
+    `(multiple-value-bind (,val ,parsed) (progn ,@parse-body)
+       (setf ,index (+ ,index ,parsed))
+       (setf ,capture-var ,val))))
+
+;; this macro isn't actually necessary but  thought it was pretty cool
+(defmacro multiple-value-setf ((&rest vars) form)
+  (let ((tag-acc))
+    (loop for i in vars do (push (gensym) tag-acc))
+    `(multiple-value-bind (,@tag-acc) ,form
+       ,@(map 'list #'(lambda (var val) `(setf ,var ,val)) vars tag-acc))))
+
+
+(defun parse (buf)
+  (unless (verify-sha-integrity buf)
+    (error "data corruption has occured. Hash digest does not check
+    out."))
+  (let ((spack (make-instance 'spack)) (i 32) (types-size) (vals-size) (vi) (types-start))
+    (parse-and-increment types-size i (leb128:decode-signed buf :start i))
+    (parse-and-increment vals-size i (leb128:decode-signed buf :start i))
+    (setf types-start i)
+    (setf vi (+ types-start types-size))
+    (loop while (< i (+ types-size types-start)) do
+         (let ((val))
+           (cond
+             ((eq (aref buf i) #x01)
+              (progn
+                (incf i)
+                (parse-and-increment val vi
+                  (leb128:decode-signed buf :start vi))
+                (spush val :integer spack)))
+             ((eq (aref buf i) #x02)
+              (progn
+                (incf i)
+                (parse-and-increment val vi
+                  (values (ieee-floats:decode-float32 (cl-intbytes:octets->int32 (subseq buf vi (+ vi 4))))
+                          4))
+                (spush val :float32 spack)))
+             ((eq (aref buf i) #x03)
+              (progn
+                (incf i)
+                (parse-and-increment val vi
+                  (values (ieee-floats:decode-float64 (cl-intbytes:octets->int64 (subseq buf vi (+ vi 8))))
+                          8))
+                (spush val :float64 spack)))
+             ((eq (aref buf i) #x04)
+              (progn
+                (incf i)
+                (spush (aref buf vi) :byte spack)
+                (incf vi)))
+             ((eq (aref buf i) #x05)
+              (progn
+                (incf i)
+                (let ((slen) (s))
+                  (parse-and-increment slen i
+                    (leb128:decode-signed buf :start i))
+                  (parse-and-increment s vi
+                    (values (subseq buf vi (+ vi slen))
+                            slen))
+                  (spush s :string spack))))
+             ((eq (aref buf i) #x10)
+              (error "Not implemented yet!"))
+             ((eq (aref buf i) #x20)
+              (error "Not implemented yet!"))
+             (t
+              (error (format nil "bad type ~A found in buffer being parsed at index ~A" (aref buf i) i))))))
+    spack))
